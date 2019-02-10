a7d5ea87dd9684505414a1e3983774ac1e0d90b4
Genric Jan Elazegui <gj.elazegui@Gmail.com>
2019-02-09 20:43:14 -0500

    Art Pass is needed

--- ScavengerHunt/Assets/Scripts/DialogManager.cs
+++ ScavengerHunt/Assets/Scripts/DialogManager.cs
@@ -7,6 +7,7 @@ public class DialogManager : MonoBehaviour
 {
 
     public TextMeshProUGUI dialogText;
+
     //Like an Array, but Queue uses FIFO
     //Think in Homestuck: John's Inventory was First in, First Out
     //It was limited, but it fits for what I am trying to do.
@@ -34,6 +35,8 @@ public class DialogManager : MonoBehaviour
 
     public void DisplayNextSentence()
     {
+		//End dialogue if there are no sentences left
+		//This is somewhat of an artifact method though-The dialogue box is always active anyway.
         if (sentences.Count == 0)
         {
             EndDialog();
@@ -41,10 +44,24 @@ public class DialogManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
+
+		
+		//Displays the sentence in game while typing it
+		StartCoroutine(TypeSentence(sentence));
         
-        //Displays the sentence in game
-        dialogText.text = sentence;
+  
+      
     }
+
+	IEnumerator TypeSentence (string sentence)
+	{
+		dialogText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogText.text += letter;
+			yield return null;
+		}
+	}
 
     public void EndDialog()
     {
--- ScavengerHunt/Assets/Scripts/DialogTrigger.cs
+++ ScavengerHunt/Assets/Scripts/DialogTrigger.cs
@@ -6,6 +6,23 @@ public class DialogTrigger : MonoBehaviour
 {
     public Dialog dialog;
     public GameObject player;
+	public GameObject gun;
+	public GameObject radio;
+	public GameObject snake;
+	
+	//This bool will help in making sure DisplayNextSentence is not spammed
+	//in update.
+	//This will start as false, which will allow DisplayNextSentence to run
+	//But will then be set to false
+	bool landmarkOne;
+	bool landmarkTwo;
+	bool landmarkThree;
+	bool treasure;
+
+	//Just for a little fun: this bool will make the treasure move away
+	bool isSpotted;
+
+	
 
     public void TriggerDialog()
     {
@@ -14,22 +31,76 @@ public class DialogTrigger : MonoBehaviour
 
     public void Start()
     {
-        player = GameObject.Find("Player");
+    
+		TriggerDialog();
+		landmarkOne = false;
+		landmarkTwo = false;
+		landmarkThree = false;
+		isSpotted = false;
 
-    }
+
+	}
 
     public void Update()
     {
-        if (Vector3.Distance(player.transform.position, transform.position) > 2f)
+      
+        if (Vector3.Distance(player.transform.position, transform.position) < 5f)
         {
-            TriggerDialog();
-        }
-        
-        if (Vector3.Distance(player.transform.position, transform.position) > 20f)
-        {
-            FindObjectOfType<DialogManager>().DisplayNextSentence();
-        }
-    }
+			if (landmarkOne == false)
+			{
+				FindObjectOfType<DialogManager>().DisplayNextSentence();
+				//Setting this landmark to true not only helps stop the functoin
+				//but it will also be used by landmarkTwo to determine to play its message
+				landmarkOne = true;
+			}
+         
+		}
 
- 
+		if (Vector3.Distance(player.transform.position, gun.transform.position) < 5f && landmarkOne == true)
+		{
+			
+			if (landmarkTwo == false )
+			{
+				FindObjectOfType<DialogManager>().DisplayNextSentence();
+				landmarkTwo = true;
+			}
+		}
+
+		if (Vector3.Distance(player.transform.position, radio.transform.position) < 5f && landmarkTwo == true)
+		{
+
+			if (landmarkThree == false)
+			{
+				FindObjectOfType<DialogManager>().DisplayNextSentence();
+				landmarkThree = true;
+			}
+		}
+
+		if (Vector3.Distance(player.transform.position, snake.transform.position) < 5f && landmarkThree == true)
+		{
+
+			if (treasure== false)
+			{
+				FindObjectOfType<DialogManager>().DisplayNextSentence();
+				treasure = true;
+			}
+
+			if (Input.GetKey(KeyCode.Space))
+			{
+				FindObjectOfType<DialogManager>().DisplayNextSentence();
+				isSpotted = true;
+			}
+
+			if (isSpotted == true)
+			{
+				snake.transform.Translate(1, 0, 0);
+			}
+		}
+
+
+
+
+	}
+
+
 }
